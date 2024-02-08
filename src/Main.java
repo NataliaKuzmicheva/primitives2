8228fc0ff8097a6399dc04c616b7fabcdb15b10a
Natalia Kuzmicheva <kuzmichyova.natalie@yandex.ru>
2024-02-08 14:17:04 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int init_account = 1500; // начальный счет
+        int initAccount = 1500; // начальный счет
         int addition = 100; // пополнение счета
         int bonus = 100; // 1 бонус за каждые 100 рублей
 
 
         if (addition >= bonus) {
-            int amount_bonus = addition/bonus;
-            System.out.println((("На ваш счет зачислено ") + (init_account + addition + amount_bonus) + (" рублей/рубля")));
-            System.out.println(("Количество бонусов = ") + (amount_bonus));
+            int amountOfBonus = addition/bonus;
+            System.out.println((("На ваш счет зачислено ") + (initAccount + addition + amountOfBonus) + (" рублей/рубля")));
+            System.out.println(("Количество бонусов = ") + (amountOfBonus));
         } else {
-            System.out.println((("На ваш счет зачислено ") + (init_account + addition) + (" рублей/рубля")));
+            System.out.println((("На ваш счет зачислено ") + (initAccount + addition) + (" рублей/рубля")));
         }
 
 
